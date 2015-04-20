f9ced8d38532b0992c2cce070ed885460afba7df
Joey Baker <joey@byjoeybaker.com>
2015-04-20 10:53:25 -0700

    Fix CLI options passing #oops

--- bin/smokestack-watch.js
+++ bin/smokestack-watch.js
@@ -5,7 +5,17 @@
 var minimist = require('minimist')
   , argv = minimist(process.argv.slice(2))
   , defaults = require('lodash.defaults')
+  , transforms = argv.t || argv.transform
+  , plugins = argv.p || argv.plugins
   , options = defaults({
+    debug: argv.d || argv.debug
+    , delay: argv.delay
+    , ignoreWatch: argv['ignore-watch']
+    , browser: argv.browser
+    , transforms: Array.isArray(transforms) ? transforms : [transforms]
+    , plugins: Array.isArray(plugins) ? plugins : [plugins]
+    , patterns: argv._
+  }, {
     debug: true
     , delay: 0
     , ignoreWatch: true
@@ -13,14 +23,6 @@ var minimist = require('minimist')
     , transforms: []
     , plugins: []
     , patterns: ['test/*.js']
-  }, {
-    debug: argv.d || argv.debug
-    , delay: argv.delay
-    , ignoreWatch: argv['ignore-watch']
-    , browser: argv.browser
-    , transforms: argv.t || argv.transform
-    , plugins: argv.p || argv.plugins
-    , patterns: argv._
   })
 
 require('../index.js')(options)
